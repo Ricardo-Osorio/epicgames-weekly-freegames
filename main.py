3db649eeb70c5d3231e168accf119d6ea9879293
Ricardo Osorio <ricardo.osorio@greenmangaming.com>
2019-12-08 19:21:40 +0000

    Update error logging and fix issue on redeem game
    Update error logging by replacing the meaning behind the CURRENT_EL
    var. Now this is named CURRENT_STEP and the message is more self
    explanatory.
    
    Fix issue when redeeming a game where the purchase button was being
    clicked too soon.
    
    Now the purchase button and 'I agree with the condition' dialog
    are inside of an await as that would break the flow.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.action_chains import ActionChains
 
 
@@ -22,7 +23,7 @@ def read_env_variables():
 
 
 def execute():
-    global CURRENT_EL
+    global CURRENT_STEP
 
     chrome_options = Options()
     chrome_options.add_argument('--headless')
@@ -34,14 +35,14 @@ def execute():
 
     try:
         # need to wait for element to be clickable
-        CURRENT_EL = 'login button'
+        CURRENT_STEP = 'find and click on login button'
         el = WebDriverWait(browser, TIMEOUT).until(
             EC.element_to_be_clickable((By.XPATH, "//a[@href='/login']"))
         )
         el.click()
 
         # need to wait for element to be clickable
-        CURRENT_EL = 'login page'
+        CURRENT_STEP = 'wait for email field on login page'
         el = WebDriverWait(browser, TIMEOUT).until(
             EC.element_to_be_clickable((By.ID, "email"))
         )
@@ -53,16 +54,18 @@ def execute():
         try:
             # login succeeded
             # need to wait for element to be clickable
+            CURRENT_STEP = 'wait for visibility of the free games'
             WebDriverWait(browser, LOGIN_TIMEOUT).until(
                 EC.visibility_of_element_located((By.XPATH, "//a[contains(@class,'StoreCard') and contains(@href,'/store/en-US/product/')]"))
             )
         except TimeoutException:
             # check if login failed
+            CURRENT_STEP = 'search for wrong credentials message'
             browser.find_element_by_xpath("//h6[contains(text(),'credentials') and contains(text(),'invalid')]")
             print('failed to login into account, credentials invalid')
             return
 
-        CURRENT_EL = 'free games page'
+        CURRENT_STEP = 'get all free games available'
         games_found = browser.find_elements_by_xpath("//a[contains(@class,'StoreCard') and contains(@href,'/store/en-US/product/')]")
         if len(games_found) < 1:
             print('no free games found')
@@ -72,46 +75,55 @@ def execute():
             free_game.click()
 
             # need to wait for element to be visible (it's not clickable if already owned)
-            CURRENT_EL = 'purchase button'
+            CURRENT_STEP = 'find the purchase button'
             purchase_button = WebDriverWait(browser, TIMEOUT).until(
                 EC.visibility_of_element_located((By.XPATH, "//button[contains(@class,'Purchase')]"))
             )
 
             # name of the game (responsive UI element)
             # will only find it when width < 1024 (default window size is 800x600 when run in headless mode)
-            CURRENT_EL = 'game title'
+            CURRENT_STEP = 'find the game title'
             name = browser.find_element_by_xpath("//h1[contains(@class,'NavigationVertical')]").text
 
             # price formated as '£11.99'
-            CURRENT_EL = 'game price'
+            CURRENT_STEP = 'find the game price'
             price = browser.find_element_by_xpath("//s").text
 
             # date formated as 'Sale ends 11/29/2019 at 3:59 PM'
-            CURRENT_EL = 'purchase button aditional info'
+            CURRENT_STEP = 'extract aditional info from the purchase button'
             expires = browser.find_element_by_xpath("//span[contains(text(),'Sale ends')]").text
 
             if purchase_button.text == "OWNED":
                 print('game \"{}\" already owned. Price was {} and {}'.format(name, price, expires))
             elif purchase_button.text == 'GET':
                 print('obtaining game ' + name)
-                purchase_button.click()
 
                 # close cookie policy span as that interferes with button click
-                CURRENT_EL = 'cookies span'
+                CURRENT_STEP = 'close the cookies banner'
                 browser.find_element_by_xpath("//button[@id='euCookieAccept']").click()
                 # scroll to button
+                CURRENT_STEP = 'scrol to purchase button and click it'
                 ActionChains(browser).move_to_element(purchase_button).perform()
+                purchase_button.click()
 
-                # purchase button
-                CURRENT_EL = 'purchase button'
-                browser.find_element_by_xpath("//button[contains(@class,'btn-primary')]").click()
+                # wait until its visible and then click the purchase button
+                CURRENT_STEP = 'find and click on the last purchase button'
+                WebDriverWait(browser, TIMEOUT).until(
+                    EC.visibility_of_element_located((By.XPATH, "//button[contains(@class,'btn-primary')]"))
+                ).click()
 
-                # click the 'I Agree" popup
-                CURRENT_EL = 'I Agree with terms dialog'
-                browser.find_elements_by_xpath("//button[contains(@class,'btn-primary')]")[1].click()
+                # wait until its visible and then click the 'I Agree" popup
+                # 'Refund and Right of Withdrawal Information' popup
+                CURRENT_STEP = 'accept the conditions of refund dialog'
+                WebDriverWait(browser, TIMEOUT).until(
+                    EC.visibility_of_all_elements_located((By.XPATH, "//button[contains(@class,'btn-primary')]"))
+                )[1].click()
 
                 # need to wait for the "thank you" message before proceding
-
+                CURRENT_STEP = 'wait for page thanking for the purchase'
+                WebDriverWait(browser, TIMEOUT).until(
+                    EC.visibility_of_element_located((By.XPATH, "//span[contains(text(),'Thank you for buying')]"))
+                )
                 print('obtained game {}. Price was {} and {}'.format(name, price, expires))
             else:
                 print('purchase button text not recognized: ' + purchase_button.text)
@@ -119,8 +131,8 @@ def execute():
             # navigate back to free games page
             browser.get('https://www.epicgames.com/store/en-US/collection/free-games-collection/')
         print('all games processed')
-    except (TimeoutException, NoSuchElementException) as ex:
-        print(CURRENT_EL + ' DOM has been updated. exception message: ' + str(ex))
+    except (TimeoutException, NoSuchElementException, WebDriverException) as ex:
+        print('failed on step \"' + CURRENT_STEP + '\". DOM may have been updated. Exception message: ' + str(ex))
         browser.close()
 
 
