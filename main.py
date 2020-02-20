e28944c31b8de8e46d6f131dc24322d51e50f3e2
Matt Morgan <matt@mattmrgn.com>
2020-02-20 16:06:06 -0500

    added email validator for credentials

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 import logging
 import pyotp
 import traceback
+import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
